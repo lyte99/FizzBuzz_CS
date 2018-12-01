b3faae8f5369acda5afcd02037e7ca32e5d5b52f
Andy Egelhof <aegelhof@gmail.com>
2018-12-01 08:40:03 -0700

    update documentation

--- Program.cs
+++ Program.cs
@@ -6,17 +6,21 @@ namespace FIzzBuzz_CS
     {
         static void Main(string[] args)
         {
-             //FIZZBUZZ!
+             //FIZZBUZZ
+             
             for  (int i=0;i<101;i++)
             {
+                //print Fizz Buzz if mutliple of 3 and 5
                 if (i % 3 == 0 && i % 5 == 0 )
                 {
-                    Console.WriteLine("FizzBuzz");
+                    Console.WriteLine("Fizz Buzz");
                 }
+                //print Fizz if mutliple of 3
                 else if (i % 3 == 0 )
                 {
                     Console.WriteLine("Fizz");
                 }
+                //print Buzz  if a multiple of 5
                 else if (i % 5 == 0 )
                 {
                    Console.WriteLine("Buzz") ;  
