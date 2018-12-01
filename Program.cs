a7041d55b9dadcf4505d62f37ed8b6a9c9239545
Andy Egelhof <aegelhof@tecsystemsgroup.com>
2018-12-01 08:31:38 -0700

    inital

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FIzzBuzz_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //FIZZBUZZ!
+            for  (int i=0;i<101;i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0 )
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0 )
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0 )
+                {
+                   Console.WriteLine("Buzz") ;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
